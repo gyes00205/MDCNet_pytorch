070c9405e169d0cb7c2d448ae8b6343bbc06493b
gyes00205 <gyes00205@gmail.com>
2022-09-14 19:16:26 +0800

    Update resume start epoch

--- finetune_only_one_network.py
+++ finetune_only_one_network.py
@@ -98,6 +98,7 @@ def main():
         if os.path.isfile(args.resume):
             log.info("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch'] + 1
             model.load_state_dict(checkpoint['state_dict'])
             optimizer.load_state_dict(checkpoint['optimizer'])
             log.info("=> loaded checkpoint '{}' (epoch {})"
